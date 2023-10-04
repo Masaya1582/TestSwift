058567085e9aebdc583791b08c0e47dd0f688de1
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-04 14:39:24 +0900

    Initial Commit

---
+++ TestSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSwift
+//
+//  Created by 中久木 雅哉(Nakakuki Masaya) on 2023/10/04.
+//  Copyright (c) 2023 ReNKCHANNEL. All rihgts reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
